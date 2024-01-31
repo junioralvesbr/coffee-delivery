3e2bd582c1aa4b3c6b2c32ff2d6471506d829438
Junior Alves <jrnalves@gmail.com>
2024-01-31 12:21:10 -0300

    remove input amout function

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,6 @@
 import { ShoppingCart } from '@phosphor-icons/react'
 import { Container, InfoPrice } from './styles'
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
 import { InputCount } from '../InputCount'
 
 type CardProps = {
@@ -16,18 +15,6 @@ type CardProps = {
 }
 
 export function Card({ coffee }: CardProps) {
-  const [amount, setAmount] = useState(0)
-
-  function decrementAmount() {
-    if (amount > 0) {
-      setAmount((old) => old - 1)
-    }
-  }
-
-  function incrementAmount() {
-    setAmount((old) => old + 1)
-  }
-
   return (
     <Container>
       <dl>
